d885529ecc06893c0f60726f64db8bfab738f7da
adriannirmalandrew <>
2022-02-03 17:59:34 +0530

    Changed skeleton function code

--- server/server.py
+++ server/server.py
@@ -25,7 +25,7 @@ def delete_user():
 	#TODO
 	return None
 
-## Post actions: Upload, Get, Delete, AddComment, DeleteComment, FileReport
+## Post actions: Upload, Get, Delete, Report
 @server.route('/uploadPost', methods = ['POST'])
 def upload_post():
 	#TODO
@@ -38,6 +38,27 @@ def get_post():
 
 @server.route('/deletePost', methods = ['POST'])
 def delete_post():
+	#TODO
+	return None
+
+@server.route('/reportPost', methods = ['POST'])
+def report_post():
+	#TODO
+	return None
+
+## Comment actions: Get, Add, Delete
+@server.route('/getComments', methods = ['GET'])
+def get_comments():
+	#TODO
+	return None
+
+@server.route('/addComment', methods = ['POST'])
+def add_comment():
+	#TODO
+	return None
+
+@server.route('/deleteComment', methods = ['POST'])
+def delete_comment():
 	#TODO
 	return None
 
