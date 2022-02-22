d87c720b3987c9b5ac7d293cfa42f6786f3b94b4
adriannirmalandrew <>
2022-02-23 01:25:20 +0530

    Created post viewer page

--- server/actions/post.py
+++ server/actions/post.py
@@ -10,6 +10,7 @@ def upload(handle, username, content, has_image):
 	upload_cur.execute('insert into posts values(%s, %s, %s, %s, %s, %s)', (post_id, username, creation_time, content, int(has_image), 0))
 	uploaded = upload_cur.rowcount
 	handle.commit()
+	upload_cur.close()
 	#Return post ID
 	if uploaded != 1:
 		return None
