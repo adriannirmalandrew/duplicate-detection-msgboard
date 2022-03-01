367f25ca7bda721082edc6ab32ff0830226a466d
adriannirmalandrew <>
2022-03-02 00:52:45 +0530

    Implemented post deletion client side code

--- www/scripts/viewPost/buttonHandlers.js
+++ www/scripts/viewPost/buttonHandlers.js
@@ -1,7 +1,30 @@
-function reportPost() {
-	//TODO
+function getPostId() {
+	let urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get("post_id");
 }
 
 function deletePost() {
+	//Get post ID
+	let postId = getPostId();
+	//Perform request
+	$.ajax({
+		url: "/action/deletePost?post_id=" + postId,
+		method: "POST",
+		statusCode: {
+			200: function(resp) {
+				alert(resp.responseText);
+				window.location.replace("/home.html");
+			},
+			401: function(resp) {
+				alert(resp.responseText + ": Not logged in!");
+			}
+			500: function(resp) {
+				alert(resp.responseText);
+			},
+		},
+	});
+}
+
+function reportPost() {
 	//TODO
 }
