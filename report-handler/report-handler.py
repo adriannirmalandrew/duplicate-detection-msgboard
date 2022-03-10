5714dae5b5d2a5f005e24e5f3ae15dc0e2371781
adriannirmalandrew <>
2022-03-11 00:45:49 +0530

    Added stub functions for finding sentence similarity

--- report-handler/report-handler.py
+++ report-handler/report-handler.py
@@ -6,25 +6,28 @@ import text_similarity
 import image_similarity
 
 def connect_db():
+	sql_handle = sqlconn.connect(host = 'localhost', user = 'project', password = 'project', database = 'socialmedia')
+	return sql_handle
+
+def get_latest_report(handle):
+	latest_cur = handle.cursor()
+	latest_cur.execute('select * from reports having ') #TODO: FINISH THIS
+
+def check_text_similarity(text1, text2):
 	return None
 
-def get_latest_report():
+def check_image_similarity(image1, image2):
 	return None
 
-def check_text_similarity():
+def mark_duplicate(handle, post_id):
 	return None
 
-def check_image_similarity():
-	return None
-
-def mark_duplicate():
-	return None
-
-def delete_report():
+def delete_report(handle, report_id):
 	return None
 
 def main():
 	#Connect to DB
+	sql_handle = connect_db()
 	#Check for new reports
 	#Get posts created previously
 	#Run text similarity check
