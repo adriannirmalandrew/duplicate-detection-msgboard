b7b15e8271545048e9dce03583746b334b912d86
adriannirmalandrew <>
2022-03-10 14:40:41 +0530

    Added skeleton code for report handler

--- report-handler/report-handler.py
+++ report-handler/report-handler.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python
 
+import mysql.connector as sqlconn
+
+import text_similarity
+import image_similarity
+
+def connect_db():
+	return None
+
+def get_latest_report():
+	return None
+
+def check_text_similarity():
+	return None
+
+def check_image_similarity():
+	return None
+
+def mark_duplicate():
+	return None
+
+def delete_report():
+	return None
+
 def main():
-	#TODO
+	#Connect to DB
+	#Check for new reports
+	#Get posts created previously
+	#Run text similarity check
+	#Run image similarity check
+	#Update DB if duplicate
+	#Delete report
 	return None
 
 if __name__ == '__main__':
