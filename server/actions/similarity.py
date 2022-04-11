286e8d56dd26412dd21732b999e74e13fa78036c
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-04-11 16:42:00 +0530

    Implemented function to get most similar post from DB

--- server/actions/similarity.py
+++ server/actions/similarity.py
@@ -3,6 +3,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#BERT model
+bert_model = None
+
+def bert_init():
+	global bert_model
+	bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+
 def tfidf(sen1, sen2):
 	vec = TfidfVectorizer()
 	corpus = [sen1, sen2]
@@ -10,7 +17,14 @@ def tfidf(sen1, sen2):
 	sim = cosine_similarity([X[0]], [X[1]])
 	return sim[0][0]
 
-def get_most_similar_post(handle, post_text):
+def bert(model, sen1, sen2):
+	#Create encodings
+	sen_vecs = model.encode([sen1, sen2])
+	#Compute cosine similarity
+	sim = cosine_similarity([sen_vecs[0]], [sen_vecs[1]])
+	return sim[0][0]
+
+def find_most_similar_post(handle, post_text):
 	sim_cur = handle.cursor(dictionary = True)
 	#Get all posts from database
 	sim_cur.execute('select * from posts')
@@ -19,7 +33,14 @@ def get_most_similar_post(handle, post_text):
 	highest_sim, most_similar = 0, db_posts[0]
 	for post in db_posts:
 		score = tfidf(post_text, post['content'])
-		#TODO: Store highest-scored post ID
+		if score > highest_sim:
+			highest_sim = score
+			most_similar = post
 	#Take most similar posts and compare using BERT
-	#Return content of post with highest similarity score
+	final_sim = 0
+	if highest_sim >= 0.8:
+		final_sim = bert(bert_model, post_text, most_similar['content'])
+	#Return post with highest similarity score
+	if final_sim > 0.8:
+		return most_similar
 	return None
