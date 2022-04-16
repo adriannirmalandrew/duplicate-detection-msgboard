282dd8d1b9aa10ab3c862fe7c05012f0d2b53dfb
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-04-16 16:31:06 +0530

    Implemented similarity comparison for tweets

--- report-handler/twitter-scraper.py
+++ report-handler/twitter-scraper.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import tweepy
+import text_similarity
 
 api_key = '255aKwPl9tlqpbTIWTA8YAswf'
 api_key_secret = '6MRd4l6tLpueXmuVB1xusGNmldlE1Qx63jSHAwaOYjoxf3XlqM'
@@ -39,11 +40,19 @@ def contains_topic_text(post_text, topics):
 # Get tweets associated with trending topics
 def get_tweets_from_topic(handle, topic):
 	results = handle.search_recent_tweets(query = topic, max_results = 15)
-	return results
+	return results.data
 
-# Compare post's text to tweets
+# Compare post's text to tweets and return similarity
 def compare_posts(post_text, tweets):
-	return None
+	highest_sim, most_similar = 0, tweets[0]
+	#Compute similarities
+	for ttw in tweets:
+		sim = text_similarity.compute(post_text, tw.text)
+		if sim > highest_sim:
+			highest_sim = sim
+			most_similar = tw
+	#Return similarity and tweet (ID and text)
+	return (highest_sim, most_similar)
 
 def main():
 	#TEST post text
@@ -61,8 +70,10 @@ def main():
 	tweepy_handle = create_tweepy_handle(bearer_token)
 	#Get search results
 	trending_tweets = get_tweets_from_topic(tweepy_handle, contained_topic)
-	#Compare post's similarity to each tweet
-	#TODO
+	print(trending_tweets)
+	#Compute post similarity
+	most_similar = compare_posts(post_text, trending_tweets)
+	print(most_similar)
 
 if __name__ == '__main__':
 	main()
