05a820ed8f7729e1dfdfcb810cc47a231f0c3497
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-04-22 11:44:16 +0530

    Fixed indentation, renamed analysis_twitter module

---
+++ server/actions/analysis_twitter.py
@@ -0,0 +1,31 @@
+## Functions to analyze a post being created by a user
+
+## Trends and sentiments helper methods
+# Get trends using Selenium
+def _get_trending_topics():
+	return None
+
+# Get trending tweets using Tweepy
+def _get_topic_tweets():
+	return None
+
+# Perform sentiment analysis on a list of tweets
+def _tweets_sentiments():
+	return None
+
+## Post similarity helper methods
+# Find most similar tweets from a list
+def _most_similar_tweets():
+	return None
+
+## Backend handler methods
+# Get trending topics+tweets and perform sentiment analysis
+def trends_and_sentiments():
+	#Get trends using Selenium
+	#Get trending tweets using Tweepy
+	#Perform sentiment analysis
+	return None
+
+# Get posts most similar to user's input
+def similar_posts():
+	return None
