b8ae0cc291c51c84263f81e902b5a7d18787dc7b
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-04-24 16:19:11 +0530

    Added analytics divs and more function stubs

--- www/scripts/home/postAnalysis.js
+++ www/scripts/home/postAnalysis.js
@@ -1,14 +1,38 @@
 //Get trending topics from Twitter and their associated sentiments
 function getTwitterTrendsSentiments() {
-    //TODO
+	//TODO
 }
 
-//Compute sentiments of user's new post
-function computeNewPostSentiment() {
-    //TODO
+//Compute sentiments of given text
+function computeNewPostSentiment(newPostContent) {
+	$.ajax({
+		url: "/action/computePostSentiment?content=" + newPostContent,
+		method: "GET",
+		statusCode: {
+			200: function(sentiment_res) {
+				return sentiment_res;
+			},
+		},
+	});
+}
+//Handle sentiment computation button
+function newPostSentimentAction() {
+	let newPostContent = $("#new-post-content").val();
+	let newPostSentiment = computeNewPostSentiment(newPostContent);
+	//Set value in document element
+	//TODO
 }
 
 //Get posts similar to user's new post
 function getSimilarPosts() {
-    //TODO
+	//TODO
 }
+//Handle similar post button
+function similarPostsAction() {
+	//TODO
+}
+
+//Get trending topics on page load
+$(document).ready(function() {
+	//TODO
+})
