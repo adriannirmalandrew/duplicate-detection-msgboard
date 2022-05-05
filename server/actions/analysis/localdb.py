3380dbf331e976e40859a1bdaf063c2b67d7a998
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-05-06 01:00:13 +0530

    Implemented finding similar posts on local DB

--- server/actions/analysis/localdb.py
+++ server/actions/analysis/localdb.py
@@ -2,7 +2,21 @@
 
 import json
 
-from actions.analysis.common import compute_sentiment, compute_similarity
+from actions.analysis.common import compute_similarity
 
-def similar_posts():
-    return None
+# Get posts similar to user's input
+def similar_posts(handle, similarity_model, post_text):
+    similar_cur = handle.cursor(dictionary = True)
+    similar_cur.execute('select * from posts')
+    post_res = similar_cur.fetchall()
+    #Compute similarity scores
+    scores = dict()
+    for post in post_res:
+        id = post['post_id']
+        content = post['content']
+        sim_score = compute_similarity(similarity_model, post_text, content)
+        if sim_score < 0:
+            continue
+        scores[id] = [content, sim_score]
+    #Return as JSON
+    return json.dumps(scores)
